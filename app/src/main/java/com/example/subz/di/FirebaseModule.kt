d1eb6b8cfc10db4c6e22ae7729ade4287ed9d442
Lisa <fa037024@gmail.com>
2026-06-21 11:02:56 +0800

    chore(di): provide firebase realtime database instance

--- app/src/main/java/com/example/subz/di/FirebaseModule.kt
+++ app/src/main/java/com/example/subz/di/FirebaseModule.kt
@@ -1,4 +1,22 @@
 package com.example.subz.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun providerFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun providerFirebaseDatabase(): FirebaseDatabase = FirebaseDatabase.getInstance()
 }
