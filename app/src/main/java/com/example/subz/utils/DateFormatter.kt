56ec0670b2c66ac7ff3e338f9e1d12f5b943cf12
Lisa <fa037024@gmail.com>
2026-06-22 19:24:09 +0800

    fix: use ISO 8601 date format to ensure accurate SQLite chronological sorting

--- app/src/main/java/com/example/subz/utils/DateFormatter.kt
+++ app/src/main/java/com/example/subz/utils/DateFormatter.kt
@@ -8,7 +8,7 @@ object DateFormatter {
     private val defaultLocale = Locale("id", "ID")
 
     fun formatToDateOnly(date: Date): String {
-        val formatter = SimpleDateFormat("dd/MM/yyyy", defaultLocale)
+        val formatter = SimpleDateFormat("yyyy-MM-dd", defaultLocale)
         return formatter.format(date)
     }
 
